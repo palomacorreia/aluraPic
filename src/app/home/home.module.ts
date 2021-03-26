92279ac44ad6f99342de16ca8514523b8ac8f3b7
Paloma <paloma.a.silva@capgemini.com>
2021-03-26 20:38:44 -0300

    * componente de login; * validação de formulário; * crendenciais para a API; * como redirecionar e obter referências do template (ViewChild); * descobrindo a plataforma de execução.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,13 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import {ReactiveFormsModule} from '@angular/forms'
-import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
-import {SignInComponent} from './signin/signin.component'
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import {SignInComponent} from './signin/signin.component';
 
 @NgModule({
     declarations: [SignInComponent],
-    imports:[CommonModule, ReactiveFormsModule,VMessageModule]
+    imports: [CommonModule, ReactiveFormsModule, VMessageModule, RouterModule]
 })
 
 export class HomeModule { }
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {  Router } from '@angular/router';
+import { AuthService } from 'src/app/core/auth.service';
 
 
 @Component({
     templateUrl: 'signin.component.html'
 })
 export class SignInComponent implements OnInit {
+    [x: string]: any;
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
-    constructor(private formBuilder: FormBuilder){}
+    constructor(private formBuilder: FormBuilder , private authService: AuthService , private router: Router) { }
 
     ngOnInit(): void {
      this.loginForm = this.formBuilder.group(
@@ -18,5 +22,24 @@ export class SignInComponent implements OnInit {
          }
      );
 
+    }
+
+    login() {
+
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => this.router.navigate(['user', userName]),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                    this.platformDetectorService.isPlatformBrowser() &&
+                        this.userNameInput.nativeElement.focus();
+                    alert('Invalid user name or password');
+                }
+            );
     }
 }
