9e5be6961c913a36dd54db00a1d42041c26e38e7
Paloma <paloma.a.silva@capgemini.com>
2021-04-05 18:16:46 -0300

    Usuário logado,AuthGuard e BehaviorSubject

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,9 +1,26 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+import { UserService } from '../user/user.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
   })
-  export class HeaderComponent  {
 
-  }
+export class HeaderComponent {
+
+    user$: Observable<User>;
+    constructor(
+        private userService: UserService,
+        private router: Router) {
+
+        this.user$ = userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
+    }
+}
