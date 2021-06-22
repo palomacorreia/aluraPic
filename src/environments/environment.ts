4de5762bf91aadba6c5e965adf3cd697773363b4
Paloma <palomacorreiadasilva@gmail.com>
2021-06-21 21:54:22 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://api-alurapic-photos.herokuapp.com',
   serverLog: 'http://localhost:7000'
 
 };
